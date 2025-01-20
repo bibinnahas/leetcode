48248b62a8b41f5e38e004fb1164aac81642a664
bbnnaz@gmail.com <bbnnaz@gmail.com>
2025-01-20 23:26:28 +1100

    day2 coding

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,13 @@
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            k += 1
+    return k
+
+
+if __name__ == '__main__':
+    print(remove_duplicates([1, 2, 3, 4, 4, 5, 6, 7, 7, 8]))
