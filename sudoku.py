43eaff0f03ba370009217e1a23afc4f4396b6e79
bbnnaz@gmail.com <bbnnaz@gmail.com>
2025-01-27 00:13:55 +1100

    sudiku

---
+++ sudoku.py
@@ -0,0 +1,35 @@
+def is_valid_sudoku(board: list[list[str]]) -> bool:
+    # Initialise 9 sets of rows, cols and boxes
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+            num = board[r][c]
+            box_index = ((r // 3) * 3) + (c // 3)
+
+            if num in rows[r] or num in cols[c] or num in boxes[box_index]:
+                return False
+            rows[r].add(num)
+            cols[c].add(num)
+            boxes[box_index].add(num)
+    return True
+
+
+if __name__ == '__main__':
+    board = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    ]
+
+    print(is_valid_sudoku(board))  # Output: True
