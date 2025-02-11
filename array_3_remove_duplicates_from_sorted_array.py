4962e311955f602dacc38776ee15652313767bdd
bbnnaz@gmail.com <bbnnaz@gmail.com>
2025-02-11 20:01:37 +1100

    leetcode - array 1 merge sorted arrays

--- array_3_remove_duplicates_from_sorted_array.py
+++ array_3_remove_duplicates_from_sorted_array.py
@@ -2,14 +2,15 @@ def array_3_remove_duplicates(nums):
     if len(nums) <= 2:
         return len(nums)
 
-    i = 1
+    i = 2
     for j in range(2, len(nums)):
-        if nums[j] != nums[1 - 1]:
-            i += 1
+        if nums[j] != nums[1 - 2]:
             nums[i] = nums[j]
-    return i + 1
+            i += 1
+    return i
 
 
 if __name__ == '__main__':
-    nums = [1, 1, 1, 2, 2, 3]
+    nums = [0, 0, 1, 1, 1, 1, 2, 3, 3]
     print(array_3_remove_duplicates(nums))
+
