6f8be379e0733c050130b8d6fe187e5e85b9827d
bbnnaz@gmail.com <bbnnaz@gmail.com>
2025-03-24 21:39:44 +1100

    array 1

---
+++ array1.py
@@ -0,0 +1,25 @@
+def merge(nums1, m, nums2, n):
+    i, j, k = m - 1, n - 1, m + n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+    if j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+
+    return nums1
+
+
+if __name__ == '__main__':
+    nums1 = [1]
+    m = 1
+    nums2 = []
+    n = 0
+
+    print(merge(nums1, m, nums2, n))
--- array_3_remove_duplicates_from_sorted_array.py
+++ array_3_remove_duplicates_from_sorted_array.py
@@ -11,5 +11,5 @@ def array_3_remove_duplicates(nums):
 
 
 if __name__ == '__main__':
-    nums = [0, 0, 1, 1, 1, 1, 2, 3, 3]
+    nums = [1,1,1,2,2,3]
     print(array_3_remove_duplicates(nums))
